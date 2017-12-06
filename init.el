5332f9872a04761a81f2f1eca010b958492ec9d8
aMasaki.Takahashi <Masaki.Takahashi11@access-company.com>
2017-12-06 18:06:18 +0900

    Add flow-js2-mode

--- init.el
+++ init.el
@@ -205,10 +205,11 @@
 
 ;; JavaScript
 (require 'js2-mode)
+(require 'flow-js2-mode)
 (autoload 'js2-mode "js2" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-hook 'js2-mode-hook (lambda () (setq js2-basic-offset 2)))
-
+(add-hook 'js2-mode-hook 'flow-js2-mode)
 
 ;; Erlang and Elixir
 (require 'elixir-mode)
