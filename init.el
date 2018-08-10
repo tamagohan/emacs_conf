4cf18918ebec6ff93145dfddf5e10f6136025c4e
aMasaki.Takahashi <Masaki.Takahashi11@access-company.com>
2018-08-10 17:25:06 +0900

    Add rainbow-delimiters

--- init.el
+++ init.el
@@ -262,3 +262,19 @@
     (if (one-window-p) (split-window))
     (other-window 1)
     (w3m-find-file html-file-name)))
+
+;; rainbow-delimiters を使うための設定
+(require 'rainbow-delimiters)
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+
+;; 括弧の色を強調する設定
+(require 'cl-lib)
+(require 'color)
+(defun rainbow-delimiters-using-stronger-colors ()
+  (interactive)
+  (cl-loop
+   for index from 1 to rainbow-delimiters-max-face-count
+   do
+   (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
+    (cl-callf color-saturate-name (face-foreground face) 30))))
+(add-hook 'emacs-startup-hook 'rainbow-delimiters-using-stronger-colors)
